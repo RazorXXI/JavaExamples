17ee80c92221332c28715737df8e2748e2248210
Mr.Hidden <juanl.romeroduenas@gmail.com>
2022-02-14 02:20:30 +0100

    Create runVariables.java

---
+++ src/Variables/runVariables.java
@@ -0,0 +1,13 @@
+package variables;
+
+public class runVariables {
+
+	public static void main(String[] args) {
+
+		Variables ejVariables = new Variables();
+		
+		ejVariables.ejemploVariablesPrimitivas();
+		ejVariables.ejemploVariablesObjeto();
+	}
+
+}
