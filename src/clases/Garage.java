2fdba7c51d772d99d9052a717a1b9e40e1fea919
RazorXXI <juanl.romeroduenas@gmail.com>
2022-03-21 21:53:11 +0100

    Update Garage.java

--- src/clases/Garage.java
+++ src/clases/Garage.java
@@ -1,25 +1,24 @@
 package clases;
 
 public class Garage {
-
 	//Declaración de Constantes
 	private static final int _NUM_PLAZAS_DEFAULT = 60;
-	
+		
 	//Declaracion de atributos
 	private Plaza[] plazas;
 	private int numPlazas;
-	
+		
 	//Declaración de métodos
-	
+		
 	//Constructor con parametros
 	public Garage(int numPlazas) {
 		this.numPlazas = numPlazas;
 		this.plazas = new Plaza[numPlazas];
 	}
-	
+		
 	//Constructor por defecto
 	public Garage() {
 		this.numPlazas = _NUM_PLAZAS_DEFAULT;
-		this.Plazas = new Plaza[numPlazas]; 
+		this.plazas = new Plaza[numPlazas]; 
 	}
 }
