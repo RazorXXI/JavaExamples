7ffc6a47de8976cd8a67faa864707dd6bffb009b
Mr.Hidden <juanl.romeroduenas@gmail.com>
2022-03-22 19:19:15 +0100

    Create CocheElectrico.java

---
+++ src/encapsulacion/CocheElectrico.java
@@ -0,0 +1,9 @@
+package encapsulacion;
+
+public interface CocheElectrico {
+    public int getCapacidadBateria();
+    public void setCapacidadBateria(int capacidadBateria);
+    public boolean getEstado();
+    public void setEstado(boolean estado);
+    public int autonomiaBateria();
+}
